c1dc16f09e800d7f3aec64c9253f882d122e7d80
smayfield <mayfield@magicworld.com>
2017-01-15 11:07:23 -0500

    added tests for message writer dependency.

---
+++ Greeter3_Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DependencyExamples;
+
+namespace Greeter1_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeIsBeforeNoon()
+        {
+            var g = new Greeter3(10); // 10am
+            var msg = g.Greet(); 
+            Assert.AreEqual("Good Morning.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsAfternoonMessage_WhenTimeBetweenNoonAndSix()
+        {
+            var g = new Greeter3(14); // 2pm
+            var msg = g.Greet(); 
+            Assert.AreEqual("Good Afternoon.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeAfterSix()
+        {
+            var g = new Greeter3(20); // 8pm
+            var msg = g.Greet(); 
+            Assert.AreEqual("Good Evening.", msg);
+        }
+    }
+}
---
+++ Greeter4_Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DependencyExamples;
+
+namespace Greeter1_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeIsBeforeNoon()
+        {
+            var mw = new MessageWriter();
+            var g = new Greeter4(10, mw); // 10am
+            var msg = g.Greet();
+            Assert.AreEqual("Good Morning.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsAfternoonMessage_WhenTimeBetweenNoonAndSix()
+        {
+            var mw = new MessageWriter();
+            var g = new Greeter4(14, mw); // 2pm
+            var msg = g.Greet();
+            Assert.AreEqual("Good Afternoon.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeAfterSix()
+        {
+            var mw = new MessageWriter();
+            var g = new Greeter4(20, mw); // 8pm
+            var msg = g.Greet();
+            Assert.AreEqual("Good Evening.", msg);
+        }
+    }
+}
