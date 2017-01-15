96c2df0103da237404675433945de3e591ecdddc
smayfield <mayfield@magicworld.com>
2017-01-15 10:59:06 -0500

    added failing unit tests for greeter1 and passing test for greeter2.

---
+++ Greeter1_Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DependencyExamples;
+
+namespace Greeter1_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeIsBeforeNoon()
+        {
+            var g = new Greeter1();
+            var msg = g.Greet();
+            Assert.AreEqual("Good Morning.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsAfternoonMessage_WhenTimeBetweenNoonAndSix()
+        {
+            var g = new Greeter1();
+            var msg = g.Greet();
+            Assert.AreEqual("Good Afternoon.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeAfterSix()
+        {
+            var g = new Greeter1();
+            var msg = g.Greet();
+            Assert.AreEqual("Good Evening.", msg);
+        }
+    }
+}
---
+++ Greeter2_Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DependencyExamples;
+
+namespace Greeter1_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeIsBeforeNoon()
+        {
+            var g = new Greeter2();
+            var msg = g.Greet(10); // 10am
+            Assert.AreEqual("Good Morning.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsAfternoonMessage_WhenTimeBetweenNoonAndSix()
+        {
+            var g = new Greeter2();
+            var msg = g.Greet(14); // 2pm
+            Assert.AreEqual("Good Afternoon.", msg);
+        }
+
+        [TestMethod]
+        public void Greet_ReturnsMorningMessage_WhenTimeAfterSix()
+        {
+            var g = new Greeter2();
+            var msg = g.Greet(20); // 8pm
+            Assert.AreEqual("Good Evening.", msg);
+        }
+    }
+}
