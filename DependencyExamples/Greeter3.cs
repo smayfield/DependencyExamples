389b32b8dafd8e9d60ab5a8f9378a3a07d45002d
smayfield <mayfield@magicworld.com>
2017-01-15 10:34:42 -0500

    added constructor injection with simple value.

---
+++ DependencyExamples/Greeter3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyExamples
+{
+    public class Greeter3
+    {
+        private int _currentHour;
+
+        public Greeter3(int currentHour)
+        {
+            _currentHour = currentHour;
+        }
+
+        public string Greet()
+        {
+            string response = "";
+            if (_currentHour < 12)
+            {
+                response = "Good Morning.";
+            }
+            else if (_currentHour < 18)
+            {
+                response = "Good Afternoon.";
+            }
+            else
+            {
+                response = "Good Evening.";
+            }
+            return response;
+        }
+    }
+}
---
+++ Greeter3-Application/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyExamples;
+
+namespace Greeter3_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new Greeter3(DateTime.Now.Hour);
+            Console.WriteLine(g.Greet());
+
+            Console.WriteLine("Press any key..");
+            Console.ReadKey();
+        }
+    }
+}
