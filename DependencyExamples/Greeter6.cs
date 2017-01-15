4894408700aba422792f480da84fb597d4f96ad0
smayfield <mayfield@magicworld.com>
2017-01-15 12:34:07 -0500

    interim checkin with unity additions... not working yet.

---
+++ DependencyExamples/Greeter6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace DependencyExamples
+{
+    public class Greeter6
+    {
+        private IMessageWriter _msgWriter;
+
+        public Greeter6([Dependency] IMessageWriter msgWriter)
+        {
+            _msgWriter = msgWriter;
+        }
+
+        public string Greet()
+        {
+            return _msgWriter.GetMessage();
+        }
+    }
+}
---
+++ Greeter6-Application/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyExamples;
+using Microsoft.Practices.Unity;
+
+namespace Greeter6_Application
+{
+    class Program
+    {
+        private IUnityContainer _container = new UnityContainer();
+
+        static void Main(string[] args)
+        {
+            var g = new Greeter6();
+            Console.WriteLine(g.Greet());
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
